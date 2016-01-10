11e775b846cc350e2f73daa56b41f8c60fa98280
Marina Corona <marina.c.corona@gmail.com>
2016-01-10 10:07:45 -0700

    deleting old method for clean phone

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -25,11 +25,6 @@ def clean_phone_number
     else
       puts phone
   end
-  
-# def clean_phone_number(homephone)
-#   homephone.to_s.rjust(10,"0")[0..9]
-#
-# end
 
 def legislators_by_zipcode(zipcode)
   Sunlight::Congress::Legislator.by_zipcode(zipcode)
